0671ab982c10a5d892c2bfa2902d4465536de584
legendsayantan <>
2023-01-01 02:36:10 +0000

    Update from https://github.com/legendsayantan/BlazorWeb/commit/da4701d8cddfaf7c5c9b92d3945d1f404eaac49d

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+﻿function animatePage() {
+    var observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+    var sections = document.querySelectorAll('.hidden');
+    sections.forEach((section) => observer.observe(section));
+    var sections2 = document.querySelectorAll('.hidden2');
+    sections2.forEach((section) => observer.observe(section));
+    var sections2 = document.querySelectorAll('.hiddenY');
+    sections2.forEach((section) => observer.observe(section));2
+    var fades = document.querySelectorAll('.fade');
+    fades.forEach((fade) => observer.observe(fade));
+    var smalls = document.querySelectorAll('.small');
+    smalls.forEach((small) => observer.observe(small));
+}
+function animatePageObserve() {
+    var callback = function (mutationsList) {
+        animatePage();
+    };
+    treeObserver(callback);
+}
+async function treeObserver(callback) {
+    // Callback function to execute when mutations are observed
+    var targetNode = document.getElementsByTagName('html')[0];
+    if (!targetNode) targetNode = document.getElementsByTagName('body')[0];
+    var config = {
+        attributes: false,
+        childList: true,
+        subtree: true
+    };
+    var observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+}
