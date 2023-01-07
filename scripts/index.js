be5a89440d82b2f612b7616e54474f1a5d1aecf6
legendsayantan <>
2023-01-07 11:25:38 +0000

    Update from https://github.com/legendsayantan/BlazorWeb/commit/58fcb9a63f5597199686153181f162698df84ef2

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
-﻿function animatePage() {
+﻿var adbInstance = null, dev = null;
+var connectBtn, nameView, container;
+function animatePage() {
     var observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
@@ -36,4 +38,37 @@ async function treeObserver(callback) {
     };
     var observer = new MutationObserver(callback);
     observer.observe(targetNode, config);
+}
+async function connectADB(connectBtnId,nameViewId,controlViewId) {
+    console.log("Connecting to WebUSB");
+    
+    let adb;
+    try {
+        webusb = await Adb.open("WebUSB");
+        document.getElementById(nameViewId).innerText = "5. Click 'Allow' in your Android device.";
+        adb = await webusb.connectAdb("host::");
+    } catch (error) {
+        console.error(error);
+    }
+    if (adb) {
+        dev = adb.transport.device;
+        connectBtn = document.getElementById(connectBtnId);
+        nameView = document.getElementById(nameViewId);
+        container = document.getElementById(controlViewId);
+        connectBtn.style.display = "none";
+        nameView.innerText = "Connected to " + dev.manufacturerName + " " + dev.productName;
+        container.style.display = "block";
+        adbInstance = adb;
+    } else {
+        document.getElementById(nameViewId).innerText = "Not connected";
+    }
+}
+async function executeOnShell(data) {
+    data = "am force-stop com.heytap.themestore; am force-stop com.nearme.themestore; " + data;
+    let shell = await adbInstance.shell(data.replace("\n",";"));
+    console.log(await shell.receive());
+    connectBtn.style.display = "block";
+    nameView.innerText = "The selected material should be unlocked.";
+    container.style.display = "none";
+    dev.close();
 }
