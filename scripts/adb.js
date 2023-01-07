6c835b82027887d9e6cbe26b02f9d688d2c58a49
legendsayantan <>
2023-01-07 11:24:51 +0000

    Update from https://github.com/legendsayantan/BlazorWeb/commit/dc5083c8082e4888193eefc4e8293f09bf53a4df

---
+++ scripts/adb.js
@@ -0,0 +1,17 @@
+﻿async function connectADB(nameViewId) {
+    console.log("Connecting to WebUSB");
+    try {
+        webusb = await Adb.open("WebUSB");
+        adb = await webusb.connectAdb("host::");
+    } catch (error) {
+        console.error(error);
+    }
+    let dev = adb.transport.device;
+    console.log(adb);
+    if (adb) {
+        document.getElementById(nameViewId).style.display = "block";
+        document.getElementById(nameViewId).innerText = "Connected to " + dev.manufacturer + " " + dev.productName;
+    } else {
+        document.getElementById(nameViewId).innerText = "Not connected";
+    }
+}
