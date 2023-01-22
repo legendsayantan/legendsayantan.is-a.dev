35005085689307a183ddfc824b774b17ee5bc910
Sayantan Paul <69689739+legendsayantan@users.noreply.github.com>
2023-01-22 19:01:54 +0530

    Update sitemap.sh

--- sitemap.sh
+++ sitemap.sh
@@ -7,6 +7,7 @@ fi
 IFS=$'\n' read -r -d '' -a allPaths < <( jq '.[] | .path' -r data/projects.json && printf '\0' )
 echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >>$filename
 for str in ${allPaths[@]}; do
+  echo "<url><loc>https://${page}${str}</loc><lastmod>${curDate}</lastmod></url>"
   echo "<url><loc>https://${page}${str}</loc><lastmod>${curDate}</lastmod></url>" >> $filename
 done
 echo '</urlset>' >> $filename
