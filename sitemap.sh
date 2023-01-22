23edb4d5512979f81293112d9cad88edf9a549aa
legendsayantan <>
2023-01-22 13:09:33 +0000

    Update from https://github.com/legendsayantan/BlazorWeb/commit/4d00703e30cd3b8bdd83fe05f9af3fa19f502ef8

---
+++ sitemap.sh
@@ -0,0 +1,12 @@
+filename='sitemap.xml'
+page= echo ${{env.GITHUB_REPOSITORY}} | cut -d '/' -f 2
+curDate=$(date +'%Y-%m-%d')
+if [ -f "$filename" ] ; then
+    rm "$filename"
+fi
+IFS=$'\n' read -r -d '' -a allPaths < <( jq '.[] | .path' -r data/projects.json && printf '\0' )
+echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' >>$filename
+for str in ${allPaths[@]}; do
+  echo "<url><loc>https://${page}${str}</loc><lastmod>${curDate}</lastmod></url>" >> $filename
+done
+echo '</urlset>' >> $filename
